7f850da5c2bce87991990ff95dc44dae4f2367da
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-07 14:34:31 -0500

    fetch 30 users at a time from Github API and able to display in UserResults element. Loading animation for async fetch.

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import loadingAnim from '../../img/loading.svg';
+import UserItem from './UserItem';
+
+function UserResults() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = async () => {
+        const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+            },
+        });
+
+        const data = await res.json();
+
+        setUsers(data);
+        setLoading(false);
+    };
+
+    // Show spinner while loading, then results when done
+    return loading ? (
+        <img src={loadingAnim} alt="" style={{ scale: '50%' }} />
+    ) : (
+        <div className="grid grid-calls-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map((user) => console.log(user))}
+        </div>
+    );
+}
+
+export default UserResults;
